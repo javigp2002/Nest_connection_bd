81737501a5bb275ca9692cf3c6b1a0bd510997b7
javigp2002 <javigp2002@gmail.com>
2023-10-23 13:01:39 +0200

    init 2.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { DbModule } from './db/db.module';
 
 @Module({
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,24 +1,24 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { MDB_CONNECTION } from './constants';
-import * as db from 'mariadb';
 import { DbConnection } from './db/db.connection';
 
 @Injectable()
 export class AppService {
   constructor(private dbConnection: DbConnection) {}
 
+  getHello(): string {
+    return 'Hello World!';
+  }
   async getUsers() {
     let conn;
     try {
       conn = await this.dbConnection.createConnection();
-
       const rows = await conn.query('SELECT * FROM users');
-      console.log(rows); //[ {val: 1}, meta: ... ]
+      // console.log(rows);
       return rows;
     } catch (err) {
       throw err;
     } finally {
-      if (conn) return conn.end();
+      if (conn) conn.end();
     }
   }
 }
